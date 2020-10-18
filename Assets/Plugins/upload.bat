09a2b8293d914b7524e34fb6eb59d42ac7c2d1f2
Larry Crochet <crolar2015@gmail.com>
2020-10-18 19:01:49 -0400

    Collider items

--- Assets/Plugins/upload.bat
+++ Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\crola\Documents\unity3d\unityprojects\GreekTempleTemplate\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1580129214047191628.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\crola\Documents\unity3d\unityprojects\GreekTempleTemplate\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1585578756775346503.json
